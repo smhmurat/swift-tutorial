303a4ca444e0602cf016422198286518fb43236b
smhmurat <smhmurat@gmail.com>
2025-01-13 23:13:23 +0300

    markup added inside playground

---
+++ 01_variables.playground/Contents.swift
@@ -0,0 +1,25 @@
+/*:
+ # Değişkenler ve Sabitler
+
+ Sabitler ve değişkenler bir adı (piSayisi veya welcomeMessage gibi) belirli bir türdeki bir değerle (3.14 sayısı veya "Hello" dizesi gibi) ilişkilendirir. Bir sabitin değeri ayarlandıktan sonra değiştirilemezken, bir değişken gelecekte farklı bir değere ayarlanabilir.
+
+ Gerçek programlar her seferinde aynı çıktıyı vermek yerine program kullanıcısının verdiği bazı girdilere dayalı sonuçlar üretme eğilimindedir. Değişken bilgisayarın belleğinde veri saklamamız, almamız ve kullanmamız için ayrılmış belirli bir parçayı temsil eden yapıdır.
+
+ Her değişkenin içinde saklayabileceğimiz bilgi türünü temsil eden bir tipi vardır. Bu tip bilgisi derleyiciye ne kadar bellek ayırması gerektiğini söyler. Bu veri türü değişken sınırlarını belirler.
+ 
+ */
+
+import UIKit
+
+var greeting = "Hello, playground"
+
+let oturumAcmaHakki = 3
+var toplamOturumAcmaDenemesi = 0
+
+let piSayisi: Double
+var a, b, c: Float
+
+var isimSoyisim = "Semih Murat"
+print(isimSoyisim)
+
+isimSoyisim = 2
